d8dfc8126df1f00ee6900be0ed63ba223541c0a6
Clayton <clayton.cox@gmail.com>
2018-10-20 13:07:46 -0400

    added user continue class and loop.

---
+++ Lab10/Continue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Continue
+    {
+        public Continue(string s)
+        {
+            StringCont = s;
+        }
+        public string StringCont { get; set; }
+
+        public bool AnswerValid()
+        {
+            Console.WriteLine("Do you wish to continue? 'yes' / 'no'");
+            StringCont = Console.ReadLine().ToLower();
+            while (true)
+            {
+
+            if (StringCont == "yes")
+                {
+                    return true;
+
+                }
+            else if(StringCont == "no")
+                {
+                    return false;
+                }
+            else
+                {
+                    Console.WriteLine("Please enter either 'yes' or 'no'");
+                    StringCont = Console.ReadLine().ToLower();
+                }
+            }
+        }
+    }
+}
--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -12,19 +12,27 @@ namespace Lab10
     {
         static void Main(string[] args)
         {
-
+            bool userCont = true;
+            int countProgramRun = 0;
             Console.WriteLine("Welcome to the Circle Tester");
 
-            Console.Write("Enter radius: ");
-            Validator check = new Validator(Console.ReadLine());
-            
+            while (userCont)
+            {
+                Console.Write("Enter radius: ");
+                Validator check = new Validator(Console.ReadLine());
 
-            Circle myCircle = new Circle(check.DoValidation());
 
-            string formattedArea = myCircle.CalculateFormattedArea();
-            string formattedCircum = myCircle.CalculateFormattedCircumference();
-            Console.WriteLine("The area is {0} and the circumference is {1}",
-                formattedArea, formattedCircum);
+                Circle myCircle = new Circle(check.DoValidation());
+
+                string formattedArea = myCircle.CalculateFormattedArea();
+                string formattedCircum = myCircle.CalculateFormattedCircumference();
+                Console.WriteLine("The area is {0} and the circumference is {1}",
+                    formattedArea, formattedCircum);
+                Continue willCont = new Continue("temp");
+                userCont = willCont.AnswerValid();
+                countProgramRun++;
+            }
+            Console.WriteLine("\nGoodbye: You created {0} Circle object(s).", countProgramRun);
             Console.ReadLine();
         }
 
